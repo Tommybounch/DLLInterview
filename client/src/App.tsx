06c9227e11db489982090e8f3aef15d3e8b039cc
Tommy Bounchanavong <ttb43@drexeul.edu>
2025-08-26 10:51:04 -0400

    Initial Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,18 @@ import './App.css';
 
 function App() {
   return (
+    //Parent Div
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Get Users Web App</h1>
+      <div className="parentContainer">
+        <div className="searchPanel">
+          <h1>Button</h1>
+          <h2>dropDown for sort/filter</h2>
+        </div>
+        <div className="resultsPanel">
+          <h1>User List</h1>
+        </div>
+      </div>
     </div>
   );
 }
