a212026f300e553d3db33b7e2256597860cc7b04
Tommy Bounchanavong <ttb43@drexeul.edu>
2025-08-26 11:44:15 -0400

    Implmented basic panels and inputs for searching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
+import { Button, Select, InputLabel, FormControl } from '@mui/material';
 import './App.css';
 
 function App() {
+  const handleSort = (event: any) => {
+    console.log('Sort Option Selected');
+  };
+
+  const handleSearch = (event: any) => {
+    console.log('Search Button Clicked');
+  };
+
   return (
-    //Parent Div
     <div className="App">
       <h1>Get Users Web App</h1>
       <div className="parentContainer">
         <div className="searchPanel">
-          <h1>Button</h1>
-          <h2>dropDown for sort/filter</h2>
+          <Button
+            variant="contained"
+            color="primary"
+            className="searchButton"
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+          <FormControl>
+            <InputLabel id="sortFilterLabel">Sort By</InputLabel>
+            <Select
+              labelId="sortFilterLabel"
+              value=""
+              id="sortFilter"
+              className="searchFilter"
+              onChange={handleSort}
+            ></Select>
+          </FormControl>
         </div>
         <div className="resultsPanel">
-          <h1>User List</h1>
+          <h2>User List</h2>
         </div>
       </div>
     </div>
