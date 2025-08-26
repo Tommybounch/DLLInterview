97d1f93fcabd7ca1c411b4e902ce659e5beecf09
Tommy Bounchanavong <ttb43@drexeul.edu>
2025-08-26 14:03:43 -0400

    Modified existing get endpoint to return pagination information and users

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,25 +1,66 @@
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
-const users = [{
-  name: 'Jorn',
-  id: 0,
-}, {
-  name: 'Markus',
-  id: 3,
-}, {
-  name: 'Andrew',
-  id: 2,
-}, {
-  name: 'Ori',
-  id: 4,
-}, {
-  name: 'Mike',
-  id: 1,
-}];
+const users = [
+  {
+    name: 'Jorn',
+    id: 0,
+  },
+  {
+    name: 'Markus',
+    id: 3,
+  },
+  {
+    name: 'Andrew',
+    id: 2,
+  },
+  {
+    name: 'Ori',
+    id: 4,
+  },
+  {
+    name: 'Mike',
+    id: 1,
+  },
+];
 
 router.get('/', (req: Request, res: Response) => {
-  res.send(users);
+  //Parse parameters
+  const size = parseInt(req.query.size as string) || 10;
+  const page = parseInt(req.query.page as string) || 1;
+  const sortField = (req.query.sortField as string) || 'id';
+
+  //Pagination
+  const totalResults = users.length;
+  const start = (page - 1) * size;
+  const end = start + size;
+
+  //Sorting
+
+  const sortedUsers = [...users].sort((a, b) => {
+    if (a[sortField as keyof typeof a] < b[sortField as keyof typeof b])
+      return -1;
+    if (a[sortField as keyof typeof a] > b[sortField as keyof typeof b])
+      return 1;
+    return 0;
+  });
+
+  const paginatedUsers = sortedUsers.slice(start, end);
+
+  const baseUrl = `${req.protocol}://${req.get('host')}${req.baseUrl}`;
+  const paging: { previous?: string; next?: string; totalResults: number } = {
+    totalResults,
+  };
+  if (page > 1) {
+    paging.previous = `${baseUrl}?size=${size}&page=${
+      page - 1
+    }&sort=${sortField}`;
+  }
+  if (end < totalResults) {
+    paging.next = `${baseUrl}?size=${size}&page=${page + 1}&sort=${sortField}`;
+  }
+
+  res.json({ data: paginatedUsers, paging });
 });
 
 export default router;
